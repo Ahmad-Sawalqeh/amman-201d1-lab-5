65258666d3892a318a28217f310926ae2eeca54b
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-10-10 12:16:19 +0300

    solving problem-1

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+// Given 4, 7 should return [11, 'The sum of 4 and7 is 11.']
+var total = a + b;
+return [total, 'The sum of ' + a + ' and ' + b + ' is ' + total + '.'];
 }
 
 // Here is the test for sum(); uncomment it to run it
